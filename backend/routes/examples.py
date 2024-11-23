b5950afa86a8c2cfcdc739181808dfb9e04615a2
Sebastian Tremblay <tremblay.se@northeastern.edu>
2024-11-23 09:41:33 -0800

    db init

--- backend/routes/examples.py
+++ backend/routes/examples.py
@@ -1,27 +1,29 @@
 from flask import Flask, jsonify
 from supabase import Client
 
-from backend.auth.authenticate_user import authenticate_user
+from auth.authenticate_user import authenticate_user
 
 # Single route function for this file, that handles all routes pertaining to
 # 'Examples' (something I made up)
+
+
 def ExamplesRoutes(app: Flask, supabase: Client):
     # Example GET route definition
     @app.route("/examples")
     def get_examples():
         examples = supabase.table("examples").select("*").execute()
         return jsonify(examples.data)
-    
+
     # Example GET route definition with authentication
     @app.route("/examples-auth")
     @authenticate_user(supabase)
     def get_examples_auth(user_id):
         examples = supabase.table("examples").select("*").execute()
         return jsonify(examples.data)
-    
 
     # Notice how there are multiple route definitions in this file, but they
     # all pertain to one feature (in this case our 'Examples' model I made up)
+
     @app.route("/examples/<id>")
     def get_example_by_id(id):
         examples = supabase.table("examples").select("*").eq("id", id).execute
--- supabase/migrations/20240910200423_initialize.sql
+++ supabase/migrations/20240910200423_initialize.sql
@@ -10,4 +10,76 @@ CREATE TABLE examples (
     some_text varchar(100) NOT NULL
 );
 
+CREATE TABLE users (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    first_name varchar(100) NOT NULL,
+    last_name varchar(100) NOT NULL,
+    email varchar(100) NOT NULL,
+    password varchar(100) NOT NULL,
+    supabase_id uuid UNIQUE NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE groups (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name varchar(100) NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE group_members (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    group_id uuid REFERENCES groups(id),
+    user_id uuid REFERENCES users(id),
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE expenses (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    group_id uuid REFERENCES groups(id),
+    amount numeric NOT NULL,
+    description varchar(100) NOT NULL,
+    paid_on timestamp NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE expense_participants (
+    expense_id uuid REFERENCES expenses(id),
+    user_id uuid REFERENCES users(id),
+    is_paid boolean NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (expense_id, user_id)
+);
+
+CREATE TABLE chores (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    group_id uuid REFERENCES groups(id),
+    name varchar(100) NOT NULL,
+    description varchar(100) NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE chore_assignments (
+    chore_id uuid REFERENCES chores(id),
+    user_id uuid REFERENCES users(id),
+    due_date timestamp NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (chore_id, user_id)
+);
+
+CREATE TABLE alarms (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id uuid REFERENCES users(id),
+    name varchar(100) NOT NULL,
+    time timestamp NOT NULL,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at timestamp DEFAULT CURRENT_TIMESTAMP
+);
+
 COMMIT;
