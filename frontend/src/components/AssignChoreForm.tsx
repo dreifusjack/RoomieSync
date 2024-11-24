1a8f5dae97a74116b2953c7b6fe2072e5d4d6ef5
dreifusjack <dreifus.j@northeastern.edu>
2024-11-24 13:17:55 -0500

    styling for chores page

--- frontend/src/components/AssignChoreForm.tsx
+++ frontend/src/components/AssignChoreForm.tsx
@@ -43,58 +43,60 @@ const AssignChoreForm: React.FC<AssignChoreFormProps> = ({
   };
 
   return (
-    <form onSubmit={handleSubmit} className="assign-chore-form">
-      <div>
-        <label>Select Chore</label>
-        <select
-          id="chore"
-          value={selectedChore}
-          onChange={(e) => setSelectedChore(e.target.value)}
-          required
-        >
-          <option value="">-- Select Chore --</option>
-          {chores.map((chore) => (
-            <option key={chore.id} value={chore.id}>
-              {chore.name}
-            </option>
-          ))}
-        </select>
-      </div>
+    <div className="form-container">
+      <form onSubmit={handleSubmit} className="form-card  ">
+        <div>
+          <label>Select Chore</label>
+          <select
+            id="chore"
+            value={selectedChore}
+            onChange={(e) => setSelectedChore(e.target.value)}
+            required
+          >
+            <option value="">-- Select Chore --</option>
+            {chores.map((chore) => (
+              <option key={chore.id} value={chore.id}>
+                {chore.name}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      <div>
-        <label htmlFor="user">Select User</label>
-        <select
-          id="user"
-          value={selectedUser}
-          onChange={(e) => setSelectedUser(e.target.value)}
-          required
-        >
-          <option value="">-- Select User --</option>
-          {users.map((user) => (
-            <option key={user.id} value={user.id}>
-              {user.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label htmlFor="dueDate">Due Date</label>
-        <input
-          type="date"
-          id="dueDate"
-          value={selectedDueDate}
-          onChange={(e) => setSelectedDueDate(e.target.value)}
-          required
-        />
-      </div>
+        <div>
+          <label htmlFor="user">Select User</label>
+          <select
+            id="user"
+            value={selectedUser}
+            onChange={(e) => setSelectedUser(e.target.value)}
+            required
+          >
+            <option value="">-- Select User --</option>
+            {users.map((user) => (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <label htmlFor="dueDate">Due Date</label>
+          <input
+            type="date"
+            id="dueDate"
+            value={selectedDueDate}
+            onChange={(e) => setSelectedDueDate(e.target.value)}
+            required
+          />
+        </div>
 
-      <button
-        type="submit"
-        disabled={!selectedChore || !selectedUser || !selectedDueDate}
-      >
-        Assign Chore
-      </button>
-    </form>
+        <button
+          type="submit"
+          disabled={!selectedChore || !selectedUser || !selectedDueDate}
+        >
+          Assign Chore
+        </button>
+      </form>
+    </div>
   );
 };
 
--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 // src/components/Sidebar.tsx
-import React from 'react';
-import Image from 'next/image';
-import styles from '../styles/Sidebar.module.css';
+import React from "react";
+import Image from "next/image";
+import styles from "../styles/Sidebar.module.css";
 
 const Sidebar: React.FC = () => {
   return (
@@ -9,11 +9,23 @@ const Sidebar: React.FC = () => {
       <ul>
         <li>
           <a href="/">
-            <Image src="/images/images.png" alt="Logo" width={150} height={100} />
+            <Image
+              src="/images/images.png"
+              alt="Logo"
+              width={150}
+              height={100}
+            />
           </a>
         </li>
-        <li><a href="/alarms">Alarms</a></li>
-        <li><a href="/expenses">Expenses</a></li>
+        <li>
+          <a href="/alarms">Alarms</a>
+        </li>
+        <li>
+          <a href="/expenses">Expenses</a>
+        </li>
+        <li>
+          <a href="/chores">Chores</a>
+        </li>
       </ul>
     </div>
   );
